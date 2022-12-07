f92b8bd46d85660f49b1bb8c6eae7ec130eb68b8
richgal <richard.gal@gmail.com>
2022-12-07 12:12:01 +0100

    helpers

--- .iex.exs
+++ .iex.exs
@@ -1,1 +1,4 @@
 process_json = ProcessProductsJson
+list_summary = fn (x) -> CheckoutFunctions.summarise_product_list(x) end
+store_product_list = ProcessProductsJson.get_product_list("./", "products.json")
+cart_list = ["VOUCHER", "TSHIRT", "VOUCHER", "VOUCHER", "MUG", "TSHIRT", "TSHIRT"]
