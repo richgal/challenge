3e3848deeb424e168365c4389744b188d85930fc
richgal <richard.gal@gmail.com>
2022-12-08 09:47:56 +0100

    checkout iterface

--- lib/challenge/checkout/checkout.ex
+++ lib/challenge/checkout/checkout.ex
@@ -1,44 +1,18 @@
 defmodule Checkout do
   @moduledoc """
-  This module implements a GenServer to keep the state individual checkout processes related carts (with product lists).
+  Interface to interact with the CheckoutServer
   """
-  use GenServer
+  alias CheckoutFunctions
 
-  def start_link([]) do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  def new(pricing_rule) do
+    cart = CheckoutFunctions.generate_new_cart(pricing_rule)
+    cart_id = CheckoutServer.call({:new_cart, cart})
+    cart_id
   end
 
-  @impl true
-  def init([]) do
-    Process.flag(:trap_exit, true)
-    {:ok, []}
-  end
-
-  def call(message) do
-    GenServer.call(__MODULE__, message)
-  end
-
-  @impl true
-  def handle_call({:new_cart, cart}, _from, state) do
-    new_state = [cart | state]
-    {:reply, cart.cart_id, new_state}
-  end
-
-  @impl true
-  def handle_call({:update_checkout_state, cart}, _from, state) do
-    new_state = CheckoutFunctions.update_checkout_state(cart, state)
-    {:reply, :ok, new_state}
-  end
-
-  @impl true
-  def handle_call({:get_cart, cart_id}, _from, state) do
-    cart = CheckoutFunctions.get_cart_from_checkout_state(cart_id, state)
-    {:reply, cart, state}
-  end
-
-  @impl true
-  def handle_call({:checkout, cart}, _from, state) do
-    new_state = [cart | state]
-    {:reply, state, new_state}
+  def scan(cart_id, product_code) do
+    cart = CheckoutServer.call({:get_cart, cart_id})
+    cart_update = CheckoutFunctions.add_product_to_cart(cart, product_code)
+    CheckoutServer.call({:update_checkout_state, cart_update})
   end
 end
