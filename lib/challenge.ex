79afede17cb72d4f6667995ad91743d170504dab
richgal <richard.gal@gmail.com>
2022-12-08 14:27:01 +0100

    delete content

--- lib/challenge.ex
+++ lib/challenge.ex
@@ -1,18 +1,2 @@
 defmodule Challenge do
-  @moduledoc """
-  Documentation for `Challenge`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Challenge.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
