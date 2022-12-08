c7ff9ac4c35724ea364b631f999e7e3243c2c09f
richgal <richard.gal@gmail.com>
2022-12-08 10:13:04 +0100

    pricing rules refactor

--- lib/challenge/pricing_rules_sets/pricing_rules.ex
+++ lib/challenge/pricing_rules_sets/pricing_rules.ex
@@ -8,7 +8,7 @@ defmodule PricingRules do
   Each pricing rule input should be a subtotal() map and should return an updated subtotal map()
   """
   alias ProcessProductsJson
-  alias CheckoutFunctions
+  alias PricingRulesCheckoutCart, as: CheckoutCart
   @type subtotal() :: map()
   @type product_price() :: float()
   @type product_code() :: String.t()
@@ -27,9 +27,9 @@ defmodule PricingRules do
 
   def pricing_rule_2_for_1(checkout_cart, product_code) do
     {_code, _name, price, quantity, _ccy} =
-      CheckoutFunctions.get_checkout_cart_element_attributes(checkout_cart, product_code)
+      CheckoutCart.get_checkout_cart_element_attributes(checkout_cart, product_code)
 
-    cart_element = CheckoutFunctions.get_checkout_cart_element(checkout_cart, product_code)
+    cart_element = CheckoutCart.get_checkout_cart_element(checkout_cart, product_code)
 
     checkout_cart =
       cond do
@@ -39,13 +39,13 @@ defmodule PricingRules do
         quantity >= 2 ->
           case rem(quantity, 2) do
             0 ->
-              CheckoutFunctions.update_checkout_cart(checkout_cart, %{
+              CheckoutCart.update_checkout_cart(checkout_cart, %{
                 cart_element
                 | "Price" => price * 0.5
               })
 
             _ ->
-              CheckoutFunctions.update_checkout_cart(checkout_cart, [
+              CheckoutCart.update_checkout_cart(checkout_cart, [
                 %{cart_element | "Price" => price * 0.5, "Quantity" => quantity - 1},
                 %{cart_element | "Quantity" => 1}
               ])
@@ -64,9 +64,9 @@ defmodule PricingRules do
 
   def pricing_rule_bulk_purchase(checkout_cart, product_code, bulk_limit, new_price) do
     {_code, _name, _price, quantity, _ccy} =
-      CheckoutFunctions.get_checkout_cart_element_attributes(checkout_cart, product_code)
+      CheckoutCart.get_checkout_cart_element_attributes(checkout_cart, product_code)
 
-    cart_element = CheckoutFunctions.get_checkout_cart_element(checkout_cart, product_code)
+    cart_element = CheckoutCart.get_checkout_cart_element(checkout_cart, product_code)
 
     checkout_cart =
       cond do
@@ -74,7 +74,7 @@ defmodule PricingRules do
           checkout_cart
 
         quantity >= bulk_limit ->
-          CheckoutFunctions.update_checkout_cart(checkout_cart, %{
+          CheckoutCart.update_checkout_cart(checkout_cart, %{
             cart_element
             | "Price" => new_price
           })
