99ea8a9f3db50b9a9c69d3163a574342597238fb
richgal <richard.gal@gmail.com>
2022-12-08 10:13:18 +0100

    CheckoutServer module name change

--- lib/challenge/application.ex
+++ lib/challenge/application.ex
@@ -8,7 +8,7 @@ defmodule Challenge.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      Checkout
+      CheckoutServer
       # Starts a worker by calling: Challenge.Worker.start_link(arg)
       # {Challenge.Worker, arg}
     ]
